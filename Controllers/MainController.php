fe874273a25b28bd3165fb363be4178697add403
edouardplantevin <plantevin.contact@gmail.com>
2021-11-28 16:15:41 +0100

    add user

--- Controllers/MainController.php
+++ Controllers/MainController.php
@@ -10,14 +10,6 @@ class MainController extends Controller
 {
     public function index()
     {
-        $server = new SuperGlobal;
-        $url = "/{$server->getServer('HTTP_HOST')}{$server->getServer('REQUEST_URI')}";
-
-        if($url == "/localhost:8888/BDDPHP/public/")
-        {
-            header('Location: ' . '/Blog/public/');
-        }
-
         $this->render('main/index', [
             'session' => new Session
         ]);
---
+++ Views/users/login.php
@@ -0,0 +1,11 @@
+
+<div class="login-container">
+    <h1>Connexion</h1>
+    
+    <div class="form-card">
+        <?= $form ?>
+        <a href="/Blog/public/users/register">Pas encore de compte ?</a>
+    </div>
+</div>
+
+
